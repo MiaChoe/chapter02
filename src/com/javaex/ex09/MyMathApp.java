ad80b882b4ade5f6889a762cf2814981521b6836
최승은 <liberalfilm@gmail.com>
2021-11-25 12:03:03 +0900

    2021.11.25(목)12:02

--- src/com/javaex/ex09/MyMathApp.java
+++ src/com/javaex/ex09/MyMathApp.java
@@ -1,9 +1,25 @@
 package com.javaex.ex09;//2021.11.25(목)11:49수업,static
 
 public class MyMathApp {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		//필드: 필요 없음
+		
+		//생성자: 디폴트생성자 사용
+		
+		//메소드gs: 필드 없음
+		
+		//메소드 일반
+		public int plusInt(int a, int b) {
+			return a+b; //int sum = a+b; 이렇게 굳이 변수 안 만들고 바로 return에 입력	
+		}
+		
+		public double plusDouble(double a, double b) {
+			return a+b;
+		}
+		
+		public double plusDoubleInt(double a, int b) {
+			double sum = a+b;
+			return a+b;
+		}
 
 	}
 
