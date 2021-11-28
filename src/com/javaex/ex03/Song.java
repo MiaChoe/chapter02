6a6e560acf93268b3ed3a6fca2bb5d0954601536
최승은 <liberalfilm@gmail.com>
2021-11-28 12:10:26 +0900

    2021.11.28(일)12:10

--- src/com/javaex/ex03/Song.java
+++ src/com/javaex/ex03/Song.java
@@ -1,4 +1,5 @@
-package com.javaex.ex03;//2021.11.22(화)11:16수업//어제자 숙제인데 불이행, this개념 적용중.
+//2021.11.28(일)12:04영상 보며 복습//2021.11.22(화)11:16수업//어제자 숙제인데 불이행, this개념 적용중.
+
 /*
 [문제]
 Song 클래스를 만드세요 Song 클래스는 다음과 같은 필드를 가지고 있습니다.
@@ -8,14 +9,26 @@ Song 클래스를 만드세요 Song 클래스는 다음과 같은 필드를 가
 노래의 작곡가를 나타내는 composer
 노래가 발표된 연도를 나타내는 year
 노래가 속한 앨범에서 트랙 번호를 나타내는 track
-1)필드의 접근을 제한하고 getter/setter 메소드를 통해 접근하세요.
-2)노래정보를 출력하는 showInfo()메소드를 작성하세요.
+	1)필드의 접근을 제한하고 getter/setter 메소드를 통해 접근하세요.
+	2)노래정보를 출력하는 showInfo()메소드를 작성하세요.
+*/
 
- */
-
+package com.javaex.ex03;
 
 public class Song {
 
+
+	
+	
+	
+	
+	
+	
+	
+	
+}
+	
+/*	
 	// 필드
 	private String title;
 	private String artist;
@@ -91,5 +104,4 @@ public class Song {
 				+ year + ", track=" + track + "]";
 	}
 	
-	
-}
+*/	
--- src/com/javaex/ex03/SongApp.java
+++ src/com/javaex/ex03/SongApp.java
@@ -1,24 +1,29 @@
-package com.javaex.ex03;//2021.11.22(화)11:47수업, 어제자 과제 불이행하여 cp
+//2021.11.28(일)12:09영상 보며 복습//2021.11.22(화)11:47수업, 어제자 과제 불이행하여
 
 /*
 [문제]
-Song 클래스를 만드세요 Song 클래스는 다음과 같은 필드를 가지고 있습니다.
-노래의 제목을 나타내는 title
-가수를 나타내는 artist
-노래가 속한 앨범 제목을 나타내는 album
-노래의 작곡가를 나타내는 composer
-노래가 발표된 연도를 나타내는 year
-노래가 속한 앨범에서 트랙 번호를 나타내는 track
-1)필드의 접근을 제한하고 getter/setter 메소드를 통해 접근하세요.
-2)노래정보를 출력하는 showInfo()메소드를 작성하세요.
+SongApp 클래스 만드세요
+	1) showInfo() 메소드를 호출하여 다음과 같이 출력하세요
+	
+	아이유, 좋은날 ( Real, 2010, 3번 track, 이민수 작곡 )
+	BIGBANG, 거짓말 ( Always, 2007, 2번 track, G-DRAGON 작곡 )
+	버스커버스커, 벚꽃엔딩 (버스커버스커1집, 2012, 4번 track, 장범준 작곡 )
+*/
 
- */
-
-public class SongApp {
+package com.javaex.ex03;
+	public class SongApp {
 
 	public static void main(String[] args) {
+
 		
 		
+		
+
+		
+	}
+
+}	
+/*		
 		//setter를 통해 정보를 메모리에 넣었는데
 		Song s1 = new Song();
 		//Song s1 = new Song("좋은 날", "아이유", "Real", "이민수", "2010", 3);
@@ -52,8 +57,6 @@ public class SongApp {
 		
 		//s3.showInfo();
 		System.out.println(s3.toString());
+*/		
 		
-		
-	}
-
-}
+	
