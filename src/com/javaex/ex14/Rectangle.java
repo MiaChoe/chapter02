a8e567edc86ab207ec118eef23b0ab53d31a5d2e
최승은 <liberalfilm@gmail.com>
2021-11-30 10:18:24 +0900

    2021.111.30(화)10:18

--- src/com/javaex/ex14/Rectangle.java
+++ src/com/javaex/ex14/Rectangle.java
@@ -26,16 +26,13 @@ public class Rectangle extends Shape {
 		return width;
 	}
 
-
 	public void setWidth(int width) {
 		this.width = width;
 	}
 
-
 	public int getHeight() {
 		return height;
 	}
-
 
 	public void setHeight(int height) {
 		this.height = height;
