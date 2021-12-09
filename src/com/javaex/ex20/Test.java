6090928a0b3507fd3d94d73c5d1bc94444541adb
최승은 <liberalfilm@gmail.com>
2021-12-09 22:43:36 +0900

    2021.12.9(목)22:44 multi-catch 『이것이 자바이!』 공부

---
+++ src/com/javaex/ex20/Test.java
@@ -0,0 +1,26 @@
+//2021.12.9(목)22:43 <이것이 자바이다!> 예제 공부
+
+package com.javaex.ex20;
+
+public class Test {
+
+	public static void main(String[] args) {
+		
+		try {
+			String data1 = args[0];
+			String data2 = args[1];
+			
+			int value1 = Integer.parseInt(data1);
+			int value2 = Integer.parseInt(data2);
+			int result = value1 + value2;
+			System.out.println(data1 + "+" + data2 + "=" + result);	
+		} catch(ArrayIndexOutOfBoundsException NumberFormatException) {
+			System.out.println("실행 매개값의 수가 부족하거나 숫자로 변환할 수 없습니다.");
+		} catch(Exception e) {
+			System.out.println("알 수 없는 예외 발생");
+		} finally {
+			System.out.println("다시 실행하세요.");
+		}
+	}
+	
+}
