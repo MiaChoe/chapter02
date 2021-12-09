5452e001cfda230d9f442a18d87b4cdace880c79
최승은 <liberalfilm@gmail.com>
2021-12-09 21:46:24 +0900

    2021.12.9(목)21:45 ArrayException, NullPointException 복습

--- src/com/javaex/ex19/ArrayException.java
+++ src/com/javaex/ex19/ArrayException.java
@@ -1,8 +1,6 @@
-//2021.11.2(목)12:06 06.예외처리 중 연습문제
+//2021.11.2(목)12:06 06.예외처리 중 연습문제, 2021.12.9(목)19:02 영상복습
 
 package com.javaex.ex19;
-
-import java.util.Scanner;
 
 public class ArrayException {
 	public static void main(String[] args) {
@@ -11,7 +9,7 @@ public class ArrayException {
 		
 		try {
 			System.out.println(intArray[3]);
-		}catch (ArrayIndexOutOfBoundsException error) {
+		}catch (ArrayIndexOutOfBoundsException error) {//이처럼 문제점이 여럿일 경우를 대비하여 catch를 여러번 쓸 수 있음
 			System.out.println(error.toString());
 		}catch (ArithmeticException error) {
 			System.out.println(error.toString());
--- src/com/javaex/ex19/NullPointException.java
+++ src/com/javaex/ex19/NullPointException.java
@@ -1,10 +1,22 @@
-//2021.11.2(목)12:!8 06.예외처리 중 연습문제
+//2021.11.2(목)12:!8 06.예외처리 중 연습문제,2021.12.9(목)21:37 영상복습
 
 package com.javaex.ex19;
 
 public class NullPointException {
 	public static void main(String[]args) {
 		
+		String str = new String("안녕하세요");
+		
+		try {
+		str = null;		
+		System.out.println(str.toString());
+		} catch (Exception error) {
+			System.out.println("주소값이 없습니다.");
+		}
+		
+	}
+}	
+		/*
 		String str = new String("안녕하세요");
 		str = null;	//주소를 끊어버리므로 찍어낸 String 인스턴스로 접근이 불가능해짐	
 				
@@ -24,3 +36,5 @@ public class NullPointException {
 	}
 	
 }
+
+*/
