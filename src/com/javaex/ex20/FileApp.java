e32db8d11a2e2fade9a811b860ce2030dd12ffed
최승은 <liberalfilm@gmail.com>
2021-12-12 13:28:58 +0900

    2021.12.12(일)13:31

--- src/com/javaex/ex20/FileApp.java
+++ src/com/javaex/ex20/FileApp.java
@@ -1,4 +1,4 @@
-//2021.12.2(목)14:20수업, 06.예외처리 중 연습문제
+//2021.12.2(목)14:20수업, 06.예외처리 중 연습문제, 2021.12.9(목)22:34 복습
 
 package com.javaex.ex20;
 
@@ -7,17 +7,14 @@ import java.io.IOException;
 public class FileApp {
 
 	public static void main(String[] args) {
-		
-		
+
 		FileManager fr = new FileManager();
-		
+
 		try {
 			fr.readFile();
 		} catch (IOException e) {
 			System.out.println("error");
 		}
-		
-
 
 	}
 
--- src/com/javaex/ex20/FileManager.java
+++ src/com/javaex/ex20/FileManager.java
@@ -1,4 +1,4 @@
-//2021.11.2(목)14:15수업: 06.예외처리 중 연습문제
+//2021.11.2(목)14:15수업: 06.예외처리 중 연습문제, 2021.12.9(목)22:34 복습
 
 package com.javaex.ex20;
 
@@ -6,27 +6,21 @@ import java.io.IOException;
 
 public class FileManager {
 
-	//필드 - 생략
-	
-	//생성자
-	//public FileManager() {}
-	
-	//메소드 g/s
-	
-	//메소드 일반
-	public void readFile() throws IOException{// c://config.txt 을 읽어오는 메소드
-		
-		//System.out.println("파일을 읽었습니다.");
-		
-	
-	    System.out.println("강제예외상황발생");
-		throw new IOException(); ////예를 들어 인터넷이 안 돼서 파일을 못 읽거나 파일이 깨져서 못 읽을 때와 같은 예외가 발생
-	
-		
-		
+	// 필드 - 생략
+
+	// 생성자
+	// public FileManager() {}
+
+	// 메소드 g/s
+
+	// 메소드 일반
+	public void readFile() throws IOException {// c://config.txt 을 읽어오는 메소드
+
+		// System.out.println("파일을 읽었습니다.");
+
+		System.out.println("강제예외상황발생");
+		throw new IOException(); //// 예를 들어 인터넷이 안 돼서 파일을 못 읽거나 파일이 깨져서 못 읽을 때와 같은 예외가 발생
+
 	}
-	
-	
+
 }
-
-
