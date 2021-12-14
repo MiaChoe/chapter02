f64c83e39d9307f0a6a22721b1483b8fbc06dbb2
최승은 <liberalfilm@gmail.com>
2021-12-15 01:58:39 +0900

    2021.12.15(수)01:56 ConstructorOverloading, this() ≒Car(String model, int Speed)

--- src/com/javaex/ex05/Song.java
+++ src/com/javaex/ex05/Song.java
@@ -1,78 +1,47 @@
-//2021.11.28(일)14:38-18:20- /
-//2021.11.24(수)10:17수업 중 어제자 과제 확인
-
-/*
- [문제]
-Song 클래스를 만드세요 Song 클래스는 다음과 같은 필드를 가지고 있습니다.
-노래의 제목을 나타내는 title
-가수를 나타내는 artist
-노래가 속한 앨범 제목을 나타내는 album
-노래의 작곡가를 나타내는 composer
-노래가 발표된 연도를 나타내는 year
-노래가 속한 앨범에서 트랙 번호를 나타내는 track
-1)필드의 접근을 제한하고 getter/setter 메소드를 통해 접근하세요.
-2)노래정보를 출력하는 showInfo()메소드를 작성하세요.
- */
-
+//2021.11.24(수)10:17수업/2021.11.28(일)14:38-18:20/Done_2021.12.15(수)01:56 ConstructorOverloading, this()(≒ Car(String model, int Speed)
 package com.javaex.ex05;
 
 public class Song {
 
-	//필드
+	// 필드
 	private String title;
 	private String artist;
 	private String album;
 	private String composer;
 	private String year;
 	private int track;
-	
-	
-	//(디폴트)생성자
+
+	// (디폴트)생성자
 	public Song() {
-		//메모리에 올리는 일
 		System.out.println("Song(0)");
 	}
-	
-	public Song(String title, String artist, String album, String composer) {
-		this(title, artist, album, composer); //맨 마지막 track자리에 공백을 주는 경우
+
+	public Song(String title, String artist, String album, String composer) {// 맨 마지막 track자리에 공백을 주는 경우
 		System.out.println("Song(4)");
 	}
-	
-	
 
 	public Song(String title, String artist, String album, String composer, String year) {
-		
-		this(title, artist, album, composer, year);
 		this.title = title;
 		this.artist = artist;
 		this.album = album;
-		this.composer = composer;//작곡가:작곡가 이름이 최대10글자까지만 입력 (코드100줄)
-		this.year = year;//년도: 올해(2021년)이후 년도가 들어오면 2021년으로 처리
+		this.composer = composer;// 작곡가:작곡가 이름이 최대10글자까지만 입력 (코드100줄)
+		this.year = year;// 년도: 올해(2021년)이후 년도가 들어오면 2021년으로 처리
 		System.out.println("Song(5)");
-		
+	}
+
+	public Song(String title, String artist, String album, String composer, String year, int track) {
+		this.title = title;
+		this.artist = artist;
+		this.album = album;
+		this.composer = composer;
+		this.year = year;
 		this.track = track;
 		System.out.println("Song(6)");
 	}
-	
-	/*
-	//Generator using Fields통해서 바로 생성, 6개 필드값 모두 초기화하는 생성자 버스커버스커 위해 새로 추가함
-	public Song(String title, String artist, String album, String composer, String year, int track) {
-		super();
-		this.title = title;
-		this.artist = artist;
-		this.album = album;
-		this.compser = composer;
-		this.year = year;
-		this.track = track;
-		System.out.println(Song(6));
-	}
 
-	*/
-	
-	//메소드-getter/setter	
-	
+	// 메소드 gs
 	public String getTitle() {
-		//return title; 아래와 같이 써도 돼. 2021.11.24(화)11:07 다만 헷갈릴 일이 없으므로 지금 주석칸처럼 많이 쓴다
+		// return title; 아래와 같이 써도 돼. 2021.11.24(화)11:07 다만 헷갈릴 일이 없으므로 지금 주석칸처럼 많이 쓴다
 		return this.title;
 	}
 
@@ -120,26 +89,24 @@ public class Song {
 		this.track = track;
 	}
 
-
-	//메소드-일반
+	// 메소드-일반
 	@Override
 	public String toString() {
-		return "Song [title=" + title + ", artist=" + artist + ", album=" + album + ", composer=" + compser + ", year="
+		return "Song [title=" + title + ", artist=" + artist + ", album=" + album + ", composer=" + composer + ", year="
 				+ year + ", track=" + track + "]";
 	}
-				
-	public void showInfo() {
-		System.out.println(artist + ", " + title + " (" +
-	album + ", " + track + "번 track, " + composer +" 작곡");
-	}
-	
-	}
-		
 
-		
-		
-		
-		
-		
-				
-				
+	public void showInfo() {
+		System.out.println(artist + ", " + title + " (" + album + ", " + track + "번 track, " + composer + " 작곡");
+	}
+
+}
+
+/*
+ * //Generator using Fields통해서 바로 생성, 6개 필드값 모두 초기화하는 생성자 버스커버스커 위해 새로 추가함
+ * public Song(String title, String artist, String album, String composer,
+ * String year, int track) { super(); this.title = title; this.artist = artist;
+ * this.album = album; this.compser = composer; this.year = year; this.track =
+ * track; System.out.println(Song(6)); }
+ * 
+ */
--- src/com/javaex/ex05/SongApp.java
+++ src/com/javaex/ex05/SongApp.java
@@ -1,64 +1,47 @@
-//2021.11.28(일)14:38영상 보며 복습
-//2021.11.24(수)11:11수업,어제자 과제 확인하면서 오늘자 this()진도. 너무 어려우면 얘는 패스하시라
-
-/*
-[문제]
-SongApp 클래스 만드세요
-1) showInfo() 메소드를 호출하여 다음과 같이 출력하세요 
- */
+//2021.11.28(일)14:38영상 보며 복습/2021.11.24(수)11:11수업,어제자 과제 확인하면서 오늘자 this()진도/Done_2021.12.15(수)01:56 ConstructorOverloading, this()(≒ Car(String model, int Speed)
 
 package com.javaex.ex05;
+
 public class SongApp {
 	public static void main(String[] args) {
-		
-		//디폴트생성자를 쓸 계획
+
+		// 디폴트생성자를 쓸 계획
 		Song s01 = new Song();
 		s01.setTitle("좋은 날");
 		s01.setArtist("아이유");
 		s01.setAlbum("Real");
 		s01.setCompser("이민수");
 		s01.setYear("2011");
-		s01.setTrack(3);		
+		s01.setTrack(3);
 		System.out.println(s01.toString());
-		
-		
-		//5개는 생성자로 초기화하고, 트랙번호는 setter로 초기화-2021.11.28(일)에 이해
-		//(=마지막 매개변수는 정해지지 않아서 정해진 다섯 개만 넣고 마지막 것은 미정이라 적지 않았다고 가정해보자)
+
+		// 5개는 생성자로 초기화하고, 트랙번호는 setter로 초기화-2021.11.28(일)에 이해
+		// (=마지막 매개변수는 정해지지 않아서 정해진 다섯 개만 넣고 마지막 것은 미정이라 적지 않았다고 가정해보자)
 		Song s02 = new Song("거짓말", "BIGBANG", "Always", "G-Dragon", "2007");
-		s02.setTrack(2); //옆과 같이 추가사항처럼 따로 기입
-		//System.out.println(s02.toString());
+		s02.setTrack(2); // 옆과 같이 추가사항처럼 따로 기입
+		// System.out.println(s02.toString());
 
 		System.out.println("--------------------------");
-		//6개 모두 필드값 초기화하는 생성자 사용(=이번엔 여섯가지를 한번에 넣으려는 s03이 생긴 경우)
+		// 6개 모두 필드값 초기화하는 생성자 사용(=이번엔 여섯가지를 한번에 넣으려는 s03이 생긴 경우)
 		Song s03 = new Song("벚꽃엔딩", "버스커버스커", "버스커버스커1집", "장범준", "2012", 4);
 		System.out.println(s03.toString());
 		System.out.println("--------------------------");
-		
+
 		Song s04 = new Song("벚꽃엔딩2", "버스커버스커2", "버스커버스커1집2", "장범준2");
 		System.out.println(s04.toString());
 		System.out.println("--------------------------");
-		
-		
-		//아이유 출력
+
+		// 아이유 출력
 		s01.showInfo();
-		
-		//빅뱅 출력
+
+		// 빅뱅 출력
 		s02.showInfo();
-		
-		//버스커버스커 출력
+
+		// 버스커버스커 출력
 		s03.showInfo();
-		
+
 		System.out.println("--------------------------");
-		Song s05 = new Song();
-		
-		
-		
-		
-		
-		
+
 	}
-	
-	
-	
 
 }
