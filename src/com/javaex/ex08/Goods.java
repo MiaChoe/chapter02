46c98c5f901f2ea3f31b3abe97243f3321c24bbe
최승은 <liberalfilm@gmail.com>
2021-12-15 02:36:32 +0900

    2021.12.15(수)02:38 미_출력은 되는데 totalCount++ 기능 몰라서 제목 미정

--- src/com/javaex/ex08/Goods.java
+++ src/com/javaex/ex08/Goods.java
@@ -1,60 +1,54 @@
-package com.javaex.ex08;//2021.11.25(목)10:22수업,static
+//2021.11.25(목)10:22수업_static
+
+package com.javaex.ex08;
 
 public class Goods {
 
-	//핃드
+	// 핃드
 	private String name;
 	private int price;
 	private int totalCount;
-	
-	
-	//셍성자
+
+	// 셍성자
 	public Goods() {
 		this.totalCount++;
 	}
-	
+
 	public Goods(String name, int price) {
 		this.name = name;
 		this.price = price;
 		this.totalCount++;
 	}
-	
-	//메소드-getter/setter
-	public void setName(String name) {
-		this.name = name;
-	}
-	
+
+	// 메소드 gs
 	public String getName() {
 		return name;
 	}
-	
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getPrice() {
+		return price;
+	}
+
 	public void setPrice(int price) {
 		this.price = price;
 	}
-	
-	public String getPrice() {
-		return price;
-	}
-	public void setTotalCount(int totalCount) {
-		this.totalCount = totalCount;
-	}
-	
-	public String getTotalCount() {
+
+	public int getTotalCount() {
 		return totalCount;
 	}
 
-	
-	
-	
-	//메소드=일반
-	
+	public void setTotalCount(int totalCount) {
+		this.totalCount = totalCount;
+	}
+
+	// 메소드 일반
 	@Override
 	public String toString() {
-		return "Goods [name=" + name + ", price=" + price + ", totalCount=" + totalCount + ", getName()=" + getName()
-				+ ", getPrice()=" + getPrice() + ", getTotalCount()=" + getTotalCount() + ", getClass()=" + getClass()
-				+ ", hashCode()=" + hashCode() + ", toString()=" + super.toString() + "]";
+		return "Goods [name=" + name + ", price=" + price + ", totalCount=" + totalCount + "]";
 	}
-	
-	
-	
+
 }
--- src/com/javaex/ex08/GoodsApp.java
+++ src/com/javaex/ex08/GoodsApp.java
@@ -1,22 +1,26 @@
-package com.javaex.ex08;//2021.11.25(목)10:22수업,static
-						//메모리에 올라가기도 전에 static쓰인 모든 게 먼저 틀이름이 static에 올라감
-						//메소드 일반 애들도 붙일 수 있음
+//2021.11.25(목)10:22수업_static
+
+package com.javaex.ex08;
 
 public class GoodsApp {
 
 	public static void main(String[] args) {
-		
-		Goods camera = new Goods("Nikon", 400000);
+
+		Goods camera = new Goods("니콘", 400000);
 		System.out.println(camera.toString());
-		
+
 		Goods cup = new Goods();
-		cup.setName("Mug Cup");
+		cup.setName("머그컵");
 		cup.setPrice(2000);
 		System.out.println(cup.toString());
-		
-		Goods computer = new Goods("LG Gram", 900000);
+
+		Goods computer = new Goods("LG 그램", 900000);
 		System.out.println(computer.toString());
-		
+
 	}
-	
+
 }
+
+/*
+ * static <1>메모리에 올라가기도 전에 static에 올라감 <2>메소드 일반 영역에도 붙일 수 있음
+ */
