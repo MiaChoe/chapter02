114890c29d5c49f305b37def27d3498e40309be2
최승은 <liberalfilm@gmail.com>
2021-12-14 23:19:32 +0900

    2021.12.14(화)23:18 getter/setter, this.'Field', 'variable'.toString()

--- src/com/javaex/ex04/Goods.java
+++ src/com/javaex/ex04/Goods.java
@@ -1,27 +1,25 @@
-package com.javaex.ex04;//2021.11.22(화)16:34수업,디폴트생성자와 복수개의생성자들을 자동으로 만들어주는 생성자using fields
+//2021.11.22(화)10:44-16:34수업_this 추가/2021.11.26(토)/Done_2021.12.14(화)23:18 getter/setter, this.'Field', 'variable'.toString()
+
+package com.javaex.ex04;
 
 public class Goods {
-	
-	//필드
+
+	// 필드
 	private String name;
 	private int price;
-	
-	
-	//생성자
-	public Goods() {
-	//메모리에 올리는 작업: 얜 밑에 애 때문에 쓰이지도 않았을 것
-	System.out.println("Goods()");
+
+	// 생성자
+	public Goods() {// 메모리에 올리는 작업. 그러나 이 생성자는 밑 생성자 때문에 쓰이지도 않았을 것
+		System.out.println("Goods()");
 	}
-	
-	public Goods(String name, int price) {
-	//메모리에 올리는 작업
-	this.name = name;
-	this.price = price;
-	System.out.println("Goods(String name, int price)");
+
+	public Goods(String name, int price) {// 메모리에 올리는 작업
+		this.name = name;
+		this.price = price;
+		System.out.println("Goods(String name, int price)");
 	}
-	
-	
-	//메소드 중 getter/setter
+
+	// 메소드 gs
 	public String getName() {
 		return name;
 	}
@@ -37,13 +35,10 @@ public class Goods {
 	public void setPrice(int price) {
 		this.price = price;
 	}
-	
-	
-	//메소드 중 일반
 
+	// 메소드 일반
 	@Override
 	public String toString() {
 		return "Goods [name=" + name + ", price=" + price + "]";
+	}
 }
-
-
--- src/com/javaex/ex04/GoodsApp.java
+++ src/com/javaex/ex04/GoodsApp.java
@@ -1,40 +1,26 @@
-package com.javaex.ex04;//2021.11.22(화)16:49수업,
+//2021.11.22(화)16:49수업/Done_2021.12.14(화)23:18 getter/setter, this.'Field', 'variable'.toString()
+
+package com.javaex.ex04;
 
 public class GoodsApp {
 
-	//필드
-	
-	
-	
-	
-	//생성자
-	
-	
-	
-	
-	//메소드 중 getter/setter
-	
-	
-	
-	
-	
-	//메소드 중 일반
+	// 필드: 생략
+
+	// 생성자: 디폴트 생성자
+
+	// 메소드 gs
+
+	// 메소드 일반
 	public static void main(String[] args) {
-		
-		Goods camera = new Goods("Nikon", 400000);
+
+		Goods camera = new Goods("니콘", 400000);
 		System.out.println(camera.toString());
-		
+
 		Goods cup = new Goods();
-		cup.setName("Mug Cup");
+		cup.setName("머그컵");
 		cup.setPrice(2000);
 		System.out.println(cup.toString());
-		
-		
-		
-		
+
 	}
-	
-	
-	
-	
+
 }
