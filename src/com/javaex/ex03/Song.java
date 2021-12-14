cb2625d1ecfdc7500657469539d57cdbf056c2b4
최승은 <liberalfilm@gmail.com>
2021-12-15 01:28:21 +0900

    2021.12.15(수)01:23 previewing:toString(),(generally)showInfo()

--- src/com/javaex/ex03/Song.java
+++ src/com/javaex/ex03/Song.java
@@ -1,18 +1,4 @@
-//2021.11.28(일)12:04영상 보며 복습//2021.11.22(화)11:16수업//어제자 숙제인데 불이행, this개념 적용중.
-//showInfo, override(to String:개발자 입장에서 빨리 확인하는 방법)
-
-/*
-[문제]
-Song 클래스를 만드세요 Song 클래스는 다음과 같은 필드를 가지고 있습니다.
-노래의 제목을 나타내는 title
-가수를 나타내는 artist
-노래가 속한 앨범 제목을 나타내는 album
-노래의 작곡가를 나타내는 composer
-노래가 발표된 연도를 나타내는 year
-노래가 속한 앨범에서 트랙 번호를 나타내는 track
-	1)필드의 접근을 제한하고 getter/setter 메소드를 통해 접근하세요.
-	2)노래정보를 출력하는 showInfo()메소드를 작성하세요.
-*/
+//2021.11.28(일)12:04영상 보며 복습//2021.11.22(화)11:16수업//어제자 숙제인데 불이행, this개념 적용중/2021.12.15(수)01:09
 
 package com.javaex.ex03;
 
@@ -28,7 +14,7 @@ public class Song {
 
 	// 생성자
 
-	// 메소드:setter,getter
+	// 메소드 gs
 	public String getTitle() {
 		return title;
 	}
@@ -79,59 +65,14 @@ public class Song {
 
 	// 메소드: 일반
 	public void showInfo() {
-		System.out.println(artist + "," + title + "( " +album +", " + year + ", " + track + "번 track, " + composer + " 작곡)");
+		System.out.println(
+				artist + "," + title + "( " + album + ", " + year + ", " + track + "번 track, " + composer + " 작곡)");
 	}
 
-	@Override //값이 잘 들어갔나 확인하는 방법, 아하
+	@Override // 값이 잘 들어갔나 확인하는 방법
 	public String toString() {
 		return "Song [title=" + title + ", artist=" + artist + ", album=" + album + ", composer=" + composer + ", year="
 				+ year + ", track=" + track + "]";
 	}
 
 }
-
-/*
- * 수업 // 필드 private String title; private String artist; private String album;
- * private String composer; private String year; private int track;
- * 
- * // 생성자
- * 
- * // 메소드: getter/setter: alt+shift+S=>겟터세터 팝업generate Getters and //
- * Setters/Ctrl+shift+F: 자동 줄 정렬
- * 
- * public String getTitle() { return title; }
- * 
- * public void setTitle(String title) { this.title = title; }
- * 
- * 
- * public String getArtist() { return artist; }
- * 
- * public void setArtist(String artist) { this.artist = artist; }
- * 
- * public String getAlbum() { return album; }
- * 
- * public void setAlbum(String album) { this.album = album; }
- * 
- * public String getComposer() { return composer; }
- * 
- * public void setComposer(String composer) { this.composer = composer; }
- * 
- * public String getYear() { return year; }
- * 
- * public void setYear(String year) { this.year = year; }
- * 
- * public int getTrack() { return track; }
- * 
- * public void setTrack(int track) { this.track = track; }
- * 
- * // 메소드: 일반 public void showInfo() { System.out.println(artist + "," + title +
- * " (" +album + ", " + year + ", " + track + "번 track, " + composer + " 작곡)");
- * 
- * }
- * 
- * 
- * @Override public String toString() { return "Song [title=" + title +
- * ", artist=" + artist + ", album=" + album + ", composer=" + composer +
- * ", year=" + year + ", track=" + track + "]"; }
- * 
- */
--- src/com/javaex/ex03/SongApp.java
+++ src/com/javaex/ex03/SongApp.java
@@ -1,20 +1,11 @@
-//2021.11.28(일)12:09영상 보며 복습//2021.11.22(화)11:47수업, 어제자 과제 불이행하여
-
-/*
-[문제]
-SongApp 클래스 만드세요
-	1) showInfo() 메소드를 호출하여 다음과 같이 출력하세요
-	
-	아이유, 좋은날 ( Real, 2010, 3번 track, 이민수 작곡 )
-	BIGBANG, 거짓말 ( Always, 2007, 2번 track, G-DRAGON 작곡 )
-	버스커버스커, 벚꽃엔딩 (버스커버스커1집, 2012, 4번 track, 장범준 작곡 )
-*/
+//2021.11.28(일)12:09영상 보며 복습//2021.11.22(화)11:47수업, 어제자 과제 불이행/2021.12.14(화)22:22 staticField·Method, finalField, staticFinal(≒constant)
 
 package com.javaex.ex03;
-	public class SongApp {
+
+public class SongApp {
 
 	public static void main(String[] args) {
-		
+
 		Song s1 = new Song();
 		s1.setTitle("좋은 날");
 		s1.setArtist("아이유");
@@ -23,7 +14,7 @@ package com.javaex.ex03;
 		s1.setYear("2010");
 		s1.setTrack(3);
 		s1.showInfo();
-		
+
 		Song s2 = new Song();
 		s2.setTitle("거짓말");
 		s2.setArtist("빅뱅");
@@ -31,53 +22,15 @@ package com.javaex.ex03;
 		s2.setYear("2007");
 		s2.setTrack(2);
 		s2.setComposer("G-Dragon");
-		
-		
+
 		Song s3 = new Song();
 		s3.setTitle("벚꽃엔딩");
 		s3.setArtist("버스커버스커");
 		s3.setAlbum("Real");
 		s3.setYear("2012");
 		s3.setTrack(4);
-		
+
 		System.out.println(s2.toString());
 	}
 
-}	
-/*		
-		//setter를 통해 정보를 메모리에 넣었는데
-		Song s1 = new Song();
-		//Song s1 = new Song("좋은 날", "아이유", "Real", "이민수", "2010", 3);
-		s1.setTitle("좋은 날");
-		s1.setArtist("아이유");
-		s1.setAlbum("Real");
-		s1.setComposer("이민수");
-		s1.setYear("2010");
-		s1.setTrack(3);
-				
-		//System.out.println(s1.getArtist());
-		s1.showInfo();
-		
-		Song s2 = new Song(); //Song s2이거를 맨 위 Song s1과 붙어 있어도 무방하다. 공간부터 내고 밑 행위 이행하는 거라서
-		s2.setTitle("거짓말");
-		s2.setArtist("BIGBANG");
-		s2.setAlbum("Always");
-		s2.setComposer("G-Dragon");
-		s2.setYear("2007");
-		s2.setTrack(2);
-		
-		s2.showInfo();
-		
-		Song s3 = new Song();
-		s3.setTitle("벚꽃엔딩");
-		s3.setArtist("버스커버스커");
-		s3.setAlbum("버스커버스커1집");
-		s3.setComposer("장범준");
-		s3.setYear("2012");
-		s3.setTrack(4);
-		
-		//s3.showInfo();
-		System.out.println(s3.toString());
-*/		
-		
-	
+}
