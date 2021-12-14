a7f6b7bdd5ea66b819a69565981d9666b8aea9e4
최승은 <liberalfilm@gmail.com>
2021-12-14 23:32:17 +0900

    2021.12.14(화)23:30 field, reference variable, instance variable, getter/setter,'variable'.set'Field'(field value);

--- src/com/javaex/ex01/Goods.java
+++ src/com/javaex/ex01/Goods.java
@@ -1,55 +1,34 @@
-/*2021.11.21(월)16:13수업//객체지향 프로그래밍 중 3.클래스의 정의 중 메소드
-2021.11.22(화)10:44수업//this 추가, 2021.11.26(토)
-
-[문제]
-쇼핑몰에서 상품을 관리하기 위해 상품관리 프로그램을 만들려고 합니다. 프로그램을 만들기 전에 업무(비즈니스) 분석을 통해 상품 객체를 분석하고 다음과 같은 Goods클래스를 정의 하였습니다. 
-
-Goods 클래스를 정의하고 GoodsApp 클래스에서  Goods 클래스를 테스트 하세요.
-  1) Goods 객체를 하나 생성하고 이 객체에 대한 레퍼런스 변수를 camera 로 합니다.
-  2) 이 객체의 데이터인 각 각의 인스턴스 변수는 다음과 같은 값을 가지도록 합니다.
-     상품이름 : “nikon”,   가격: 400000
-  3) 값을 세팅 한 후,  객체의 데이터를 출력해 보세요.
-  
-다음의 데이터를 추가한 후, 출력해 보세요
-상품이름 : “니콘”,    가격: 400000
-상품이름 : “LG그램”,    가격: 900000
-상품이름 : “머그컵”,    가격: 2000
- 
-*/
+//2021.11.21(월)16:13수업_클래스의 정의 중 메소드/Done_2021.12.14(화)23:05 field, reference variable, instance variable, getter/setter,'variable'.set'Field'(field value);
 
 package com.javaex.ex01;
 
 public class Goods {
 
-	//필드 영역
+	// 필드 영역
 	String name;
 	int price;
-	
-	//메소드 정의하는 영역
+
+	// 메소드 정의하는 영역
 	public void setName(String name) {
 		this.name = name;
 	}
-	
+
 	public String getName() {
 		return name;
 	}
-	
+
 	public void setPrice(int price) {
 		this.price = price;
 	}
-	
+
 	public int getPrice() {
 		return price;
 	}
-	
+
 	public void showInfo() {
 		System.out.println("상품이름: " + name);
 		System.out.println("가격: " + price);
 		System.out.println("---------------------");
 	}
-	
-	
 
-	
-	
 }
--- src/com/javaex/ex01/GoodsApp.java
+++ src/com/javaex/ex01/GoodsApp.java
@@ -1,62 +1,45 @@
-package com.javaex.ex01;//2021.11.21(월)14:00수업//Chap02.객체지향 프로그래밍 중 3.클래스의 정의 중 클래스의 구조, 필드
-						//Done//2021.11.25(목)19:08 복습
-/*
-[문제]
-쇼핑몰에서 상품을 관리하기 위해 상품관리 프로그램을 만들려고 합니다. 프로그램을 만들기 전에 업무(비즈니스) 분석을 통해 상품 객체를 분석하고 다음과 같은 Goods클래스를 정의 하였습니다. 
+//2021.11.21(월)14:00수업_클래스의 구조 중 필드/Done_2021.11.25(목)19:08복습/2021.12.14(화)field, reference variable, instance variable, getter/setter,'variable'.set'Field'(field value);
 
-Goods 클래스를 정의하고 GoodsApp 클래스에서  Goods 클래스를 테스트 하세요.
-  1) Goods 객체를 하나 생성하고 이 객체에 대한 레퍼런스 변수를 camera 로 합니다.
-  2) 이 객체의 데이터인 각 각의 인스턴스 변수는 다음과 같은 값을 가지도록 합니다.      상품이름 : “nikon”,   가격: 400000
-  3) 값을 세팅 한 후,  객체의 데이터를 출력해 보세요.
- */
-
+package com.javaex.ex01;
 
 public class GoodsApp {
 
 	public static void main(String[] args) {
-		
+
 		Goods camera = new Goods();
-		camera.setName("Nikon");
+		camera.setName("니콘");
 		camera.setPrice(400000);
-		
-		/*
-		System.out.println(camera.getName());
-		System.out.println(camera.getPrice());
-		*/
-		
+
 		camera.showInfo();
-		
-		Goods computer = new Goods (); 
-		computer.setName("LG Gram");
+
+		Goods computer = new Goods();
+		computer.setName("LG 그램");
 		computer.setPrice(900000);
-		
+
 		computer.showInfo();
-		
-		//computer LG Gram
-		
-		
+
+		// computer LG Gram
+
 		/*
-		Goods camera = new Goods(); //붕어빵 틀에 해당하는 애는 여기서 딱 찍어주고 다시 하드디스크에 잘 있다. 주소형 이름은 힙에 따라가면 나오는 이름으로
-		camera.name = "Nikon";//틀에 밀가루 반죽 넣고 슈크림을 넣을 수도 있었지만 팥을 넣은 격
-		camera.price = 400000;
-		
-		Goods computer = new Goods();//붕어빵 틀 찍고 다시 하드디스크로 틀은 돌아간 상황. 그런데 이러면 아무도 접근하지 못함. 그래서 대입할 곳 변수와 자료형 적자
-		computer.name = "LG Gram";
-		computer.price = 900000;
-		
-		Goods cup = new Goods();
-		cup.name = "Mug Cup";
-		cup.price = 2000;
-		
-		System.out.println(camera.name);
-		System.out.println(camera.price);
-		
-		System.out.println(computer.name);
-		System.out.println(computer.price);
-		
-		System.out.println(cup.name);
-		System.out.println(cup.price);
-		*/
+		 * System.out.println(camera.getName()); System.out.println(camera.getPrice());
+		 */
+
+		/*
+		 * Goods camera = new Goods(); //붕어빵 틀에 해당하는 애는 여기서 딱 찍어주고 다시 하드디스크에 잘 있다. 주소형
+		 * 이름은 힙에 따라가면 나오는 이름으로 camera.name = "Nikon";//틀에 밀가루 반죽 넣고 슈크림을 넣을 수도 있었지만 팥을
+		 * 넣은 격 camera.price = 400000;
+		 * 
+		 * Goods computer = new Goods();//붕어빵 틀 찍고 다시 하드디스크로 틀은 돌아간 상황. 그런데 이러면 아무도 접근하지
+		 * 못함. 그래서 대입할 곳 변수와 자료형 적자 computer.name = "LG Gram"; computer.price = 900000;
+		 * 
+		 * Goods cup = new Goods(); cup.name = "Mug Cup"; cup.price = 2000;
+		 * 
+		 * System.out.println(camera.name); System.out.println(camera.price);
+		 * 
+		 * System.out.println(computer.name); System.out.println(computer.price);
+		 * 
+		 * System.out.println(cup.name); System.out.println(cup.price);
+		 */
 	}
 
 }
