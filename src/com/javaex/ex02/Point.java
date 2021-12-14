e90eef027db55944c82ec1fd1bd021e62a6f67f3
최승은 <liberalfilm@gmail.com>
2021-12-15 01:04:50 +0900

    2021.12.15(수)01:06 binding plural parameters

--- src/com/javaex/ex02/Point.java
+++ src/com/javaex/ex02/Point.java
@@ -1,147 +1,85 @@
-package com.javaex.ex02;//2021.11.21(화)09:56수업
-						//2021.11.24(수)13:53, boolean넣은 draw
-						//2021.11.26(금)00:47수정
-						//2021.11.26(금)22:45녹화 들으며 수정: draw이해, this.수업듣기
-/*
-[문제]
-Point 클래스를 만드세요
-	1) x, y 좌표를 나타낼 수 있는 필드 작성
-	2) x, y 좌표에 접근할 수 있는 getter/setter 메소드 작성
-	3) 다음 실행 결과를 참조하여 draw()메소드 작성
- */
+//2021.11.21(화)09:56수업, 2021.11.24(수)13:53_boolean넣은 draw/2021.11.26(금)22:45녹화 들으며 수정: draw이해, this.수업듣기/2021.12.14(화)23:43
 
+package com.javaex.ex02;
 
 public class Point {
-	
-	//필드
-	private int x; //3
-	private int y; //6
-	
-	
-	//생성자
-		
-	//메소드: 메소드를 통해 불러올 거야, draw가 있어서 막혀있어
-	public void setX(int x) { 
+
+	// 필드
+	private int x; // 3
+	private int y; // 6
+
+	// 생성자
+
+	// 메소드: 메소드를 통해 불러올 거야, draw가 있어서 막혀있어
+	public void setX(int x) {
 		this.x = x;
 	}
-	
+
 	public int getX() {
 		return x;
 	}
-	
+
 	public void setY(int y) {
 		this.y = y;
 	}
-	
+
 	public int getY() {
 		return y;
 	}
-	
-	public void draw() {//자기자신의 필드값을 읽을 거니까 줄 필요 없어, 갖고 있는 거니까.
-		System.out.println("점[x=" + x + ", y=" + y +"]을 그렸습니다.");
+
+	// 메소드 일반
+	public void draw() {// 자기자신의 필드값을 읽을 거니까 줄 필요 없어, 갖고 있는 거니까.
+		System.out.println("점[x=" + x + ", y=" + y + "]을 그렸습니다.");
 	}
-	
+
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 /*
-public class Point {
-
-	//필드
-	private int x;
-	private int y;
-
-	/*
-	//생성자
-	public Point() {
-		System.out.println("Point()실행");
-	}
-	*/
-/*	
-	public Point(int x, String name) {
-		//해당클래스를 메모리에 올리는 일
-		this.x = x;
-		System.out.println("Point(1)");
-	}
-	
-	//아래와 같이는 못하는데 위의 int x와 자료형까지 같기 때문이다
-	public Point(int y, String name) {
-		//해당클래스를 메모리에 올리는 일
-		this.y = y;
-	}
-	
-	
-	
-	
-	public Point(int x, int y) {
-		//위와 같이 쓰면 무조건 해당클래스를 메모리에 올려둠. setter가 했던 일을 똑같이 해달라고 이제 밑에 일 시킬 차례
-		this.x = x;
-		this y = y;
-		System.out.println("Point(2)");
-	}
-	
-	
-	
-	//메소드: 필드의 x와 밖에서 받은 x를 잘 구분해두는 것도 중요함. 하다보면 필드의 변수와 메소드 새 변수 같은 게 ㅈ홓아
-	public void setX(int x); {
-		if(x>=0) {
-		this.x = x;	
-		} 
-		else {
-		this.x = 0;
-		}
-	}	
-	
-	//값을 읽어오는 거라서 특별히 불러올 게 없음
-	public int getX() {
-		return x;
-	}
-	
-	public int setY(int y) {
-		this.y = y;
-	}
-	
-	public int getY() {
-		return y;
-	}
-	
-	public void draw() {
-		System.out.println("점[x= "+ x +", " +"  y =" + y + " 을 그렸습니다.");
-	}
-		
-	
-	//action=true 그리는 기능, action=false면 지우는 기능
-	public void draw(boolean action) {
-		if(action == true) {
-			System.out.println("점[x=" + x + ", y=" + y + "]을 그렸습니다");
-		}
-		else if(action == false) {
-			System.out.println("점[x=" + x + ", y=" + y + "]을 그렸습니다");
-		}
-		else {
-				System.out.println("점[x=" + x + ", y=" + y + "]을 그렸습니다");	
-		}	
-	}
-	
-	
-	@override
-	public String toString() {
-		return "Point[x="+x+", "
-		
-	}
-	
-}
-*/
+ * public class Point {
+ * 
+ * //필드 private int x; private int y;
+ * 
+ * /* //생성자 public Point() { System.out.println("Point()실행"); }
+ */
+/*
+ * public Point(int x, String name) { //해당클래스를 메모리에 올리는 일 this.x = x;
+ * System.out.println("Point(1)"); }
+ * 
+ * //아래와 같이는 못하는데 위의 int x와 자료형까지 같기 때문이다 public Point(int y, String name) {
+ * //해당클래스를 메모리에 올리는 일 this.y = y; }
+ * 
+ * 
+ * 
+ * 
+ * public Point(int x, int y) { //위와 같이 쓰면 무조건 해당클래스를 메모리에 올려둠. setter가 했던 일을
+ * 똑같이 해달라고 이제 밑에 일 시킬 차례 this.x = x; this y = y;
+ * System.out.println("Point(2)"); }
+ * 
+ * 
+ * 
+ * //메소드: 필드의 x와 밖에서 받은 x를 잘 구분해두는 것도 중요함. 하다보면 필드의 변수와 메소드 새 변수 같은 게 ㅈ홓아 public
+ * void setX(int x); { if(x>=0) { this.x = x; } else { this.x = 0; } }
+ * 
+ * //값을 읽어오는 거라서 특별히 불러올 게 없음 public int getX() { return x; }
+ * 
+ * public int setY(int y) { this.y = y; }
+ * 
+ * public int getY() { return y; }
+ * 
+ * public void draw() { System.out.println("점[x= "+ x +", " +"  y =" + y +
+ * " 을 그렸습니다."); }
+ * 
+ * 
+ * //action=true 그리는 기능, action=false면 지우는 기능 public void draw(boolean action) {
+ * if(action == true) { System.out.println("점[x=" + x + ", y=" + y +
+ * "]을 그렸습니다"); } else if(action == false) { System.out.println("점[x=" + x +
+ * ", y=" + y + "]을 그렸습니다"); } else { System.out.println("점[x=" + x + ", y=" + y
+ * + "]을 그렸습니다"); } }
+ * 
+ * 
+ * @override public String toString() { return "Point[x="+x+", "
+ * 
+ * }
+ * 
+ * }
+ */
--- src/com/javaex/ex02/PointApp.java
+++ src/com/javaex/ex02/PointApp.java
@@ -1,60 +1,31 @@
-package com.javaex.ex02;//2021.11.22(화)14:55수업,
-						//해당클래스를 메모리에 올리는 일이 주된+return을 쓰지 않는 생성자
-						//2021.11.26(금)00:47수정
-						//2021.11.26(금)22:42녹화 들으며 수정
-/*
-[문제]
-PointApp 클래스 만드세요
-	1) draw() 메소드를 호출하여 다음과 같이 출력하세요
-	2) 점[x=5, y=5]을 그렸습니다. / 점[x=10, y=23]을 그렸습니다.
-*/
+//2021.11.22(화)14:55수업,2021.11.26(금)00:47수정,2021.11.26(금)22:42녹화 들으며 수정/2021.12.14(화)23:40
 
+package com.javaex.ex02;
 
 public class PointApp {
 	public static void main(String[] args) {
-	
+
 		Point p01 = new Point();
 		p01.setX(3);
 		p01.setY(6);
-		p01.draw(); //한번에 출력하는 거 만들자
-		
-		//System.out.println(p01.getX());
-		
+		p01.draw(); // 한번에 출력하는 거 만들자
+
+		// System.out.println(p01.getX());
+
 		Point p02 = new Point();
 		p02.setX(7);
 		p02.setY(7);
 		p02.draw();
-		
-		}
-		
-}	
-	
-	/*
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
-		Point p01 = new Point(3, 6);//Point(2) setter를 이용하지 않고 바로 올리는 법
-		/*
-		p01.setX(3);
-		p01.setY(6);
-		*/
-		
-		/*
-		p01.draw();
-		*/
-		
-	/*
-		Point p02 = new Point(); //Point()
-		p02.setX(7);
-		p02.setY(7);
-		p02.draw();
-		
-		
-		Point p03 = new Point(100); //Point(1)
-		p03.setY(101);
-		p03.draw();
-		
-		
-	 */		
+	}
 
+}
 
+/*
+ * 생성자: 1>메모리에 올리는 일을 주로 한다. 2>return을 가질 수 없다.
+ * 
+ * setter를 이용하지 않고 바로 올리는 법
+ * public static void main(String[]args) {
+ * 		Point p01 = new Point(3, 6);
+ *		}
+ */
